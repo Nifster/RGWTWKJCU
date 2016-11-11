89570cbd60d8235e887d3ab44cb76fa43cd280c0
nifster <h_ghazali@hotmail.com>
2016-11-12 02:36:25 +0800

    project import

---
+++ FriendshipJCU/Assets/Scripts/SpriteOrder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteOrder : MonoBehaviour {
+
+	SpriteRenderer spriteRenderer;
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = this.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spriteRenderer.sortingOrder = (int)Camera.main.WorldToScreenPoint (spriteRenderer.bounds.min).y * -1;
+		//moves the object sprite back or front based on y values;
+	}
+}
