ef730afbc1489c8875927f66a2f402ca0228a055
yeojoey <yeojoey@gmail.com>
2016-11-12 03:27:55 +0800

    Now

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public GameObject player;
+
+	public float xMin, xMax, yMin, yMax;
+
+	private Vector3 offset;
+
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate () {
+
+		Vector3 pos = player.transform.position + offset;
+
+		transform.position = new Vector3 (
+			Mathf.Clamp (pos.x, xMin, xMax),
+			Mathf.Clamp (pos.y, yMin, yMax),
+			pos.z
+		);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public int PlayerNo;
+	public float speed;
+
+	private Rigidbody2D rb2d;
+	private Vector2 prevMovement;
+	private string horzAxis, vertAxis;
+	private string attackAnimName;
+	private KeyCode upKey, downKey, leftKey, rightKey, attackKey;
+	private Animator animator;
+
+	private GameObject sword;
+
+	void Start () {
+
+		sword = this.GetComponent<GameObject> ();
+		animator = this.GetComponent<Animator> ();
+
+		rb2d = GetComponent<Rigidbody2D> ();
+		if (PlayerNo == 2) {
+			horzAxis = "HorizontalP2";
+			vertAxis = "VerticalP2";
+			upKey = KeyCode.UpArrow;
+			downKey = KeyCode.DownArrow;
+			leftKey = KeyCode.LeftArrow;
+			rightKey = KeyCode.RightArrow;
+			attackKey = KeyCode.Keypad1;
+			attackAnimName = "player2_attack";
+		} else {
+			horzAxis = "Horizontal";
+			vertAxis = "Vertical";
+			upKey = KeyCode.W;
+			downKey = KeyCode.S;
+			leftKey = KeyCode.A;
+			rightKey = KeyCode.D;
+			attackKey = KeyCode.F;
+			attackAnimName = "player1_attack";
+		}
+
+		sword.SetActive (false);
+
+	}
+
+	void Update () {
+
+		if (Input.GetKey (upKey) || Input.GetKey (downKey) || Input.GetKey (leftKey) || Input.GetKey (rightKey)) {
+			animator.SetBool ("isMoving", true);
+		} else {
+			animator.SetBool ("isMoving", false);
+		}
+
+		if (Input.GetKeyDown (attackKey)) {
+			animator.SetBool ("isAttacking", true);
+		} else {
+			animator.SetBool ("isAttacking", false);
+		}
+
+		if (AnimatorIsPlaying (attackAnimName)) {
+			sword.SetActive (true);
+		} else {
+			sword.SetActive (false);
+		}
+
+
+	}
+
+	void FixedUpdate () {
+
+		float moveHorizontal = Input.GetAxis (horzAxis);
+		float moveVertical = Input.GetAxis (vertAxis);
+
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+		rb2d.AddForce (movement * speed);
+
+	}
+
+
+	bool AnimatorIsPlaying(){
+		return animator.GetCurrentAnimatorStateInfo(0).length > animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+	}
+
+	bool AnimatorIsPlaying(string stateName){
+		return AnimatorIsPlaying() && animator.GetCurrentAnimatorStateInfo(0).IsName(stateName);
+	}
