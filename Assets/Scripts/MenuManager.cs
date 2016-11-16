4cc210616f62f2cd545c6706b6450406827821d1
nifster <h_ghazali@hotmail.com>
2016-11-16 20:06:16 +0800

    Final commit with title screen and game over screen done

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
-
+using System.Collections.Generic;
+using UnityEngine.UI;
 public class MenuManager : MonoBehaviour {
 
+    public List<Sprite> Cutscene;
+    public List<string> texts;
+    int index = 0;
+
     public GameObject creditsPanel;
+    public GameObject cutscenesPanel;
+    public GameObject cutsceneImage;
+    public GameObject cutsceneText;
 
 	// Use this for initialization
 	void Start () {
@@ -18,7 +26,23 @@ public class MenuManager : MonoBehaviour {
 
     public void StartGame()
     {
-        SceneManager.LoadScene("main");
+        cutscenesPanel.SetActive(true);
+        //SceneManager.LoadScene("main");
+    }
+
+    public void cutscenesButton()
+    {
+        index++;
+        if (index > 2)
+        {
+            SceneManager.LoadScene("main");
+        }
+        else
+        {
+            cutsceneImage.GetComponent<Image>().sprite = Cutscene[index];
+            cutsceneText.GetComponent<Text>().text = texts[index];
+            
+        }
     }
 
     public void QuitGame()
